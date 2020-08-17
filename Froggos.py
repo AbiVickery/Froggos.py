3fbd42394c8a9dddd6bb93b4621fd6f1abd605cf
Abi Vickery <vickeryabi@gmail.com>
2020-08-17 15:27:50 -0500

    syntax

--- Froggos.py
+++ Froggos.py
@@ -11,7 +11,7 @@ def froggo(x,y):
     eye = c.create_oval (x + 70, y - 80, x + 130, y - 20, fill='white')
     eyeball = c.create_oval (x + 90, y - 60, x + 110, y - 40, fill='black')
     mouth = c.create_oval (x + 50, y + 70, x + 150, y + 90, fill='red')
-    neck = c.create_line(x + 1--, y, x + 100, y - 20)
+    neck = c.create_line(x + 100, y, x + 100, y - 20)
 
 while True:
     x0 = randint(0, size)
