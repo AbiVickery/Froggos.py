f20dc96024874d3617e570dacc5eedf29a59bb04
Abi Vickery <vickeryabi@gmail.com>
2020-08-17 15:27:05 -0500

    froggos

---
+++ Froggos.py
@@ -0,0 +1,21 @@
+from tandom import *
+from tkinter import *
+size = 735
+window = Tk()
+window.title('Froggos')
+c = Canvas(window, width=size, height=size)
+c.pack()
+
+def froggo(x,y):
+    body = c.create_oval(x, y, x + 200, y + 100, fill='purple')
+    eye = c.create_oval (x + 70, y - 80, x + 130, y - 20, fill='white')
+    eyeball = c.create_oval (x + 90, y - 60, x + 110, y - 40, fill='black')
+    mouth = c.create_oval (x + 50, y + 70, x + 150, y + 90, fill='red')
+    neck = c.create_line(x + 1--, y, x + 100, y - 20)
+
+while True:
+    x0 = randint(0, size)
+    y0 = randint(0, size)
+    d = randint(0, size/5)
+    froggo(x0,y0)
+    window.update()
